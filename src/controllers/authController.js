b7791350282b6afd1fc5bdbb6db8ddb4605da2b6
Max Alva <alva85@gmail.com>
2021-08-02 13:21:30 -0500

    Registro de usuarios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,37 @@
 const { response, request } = require( 'express' );
+const User = require( '../models/user' );
 
-const register = ( req = request, res = response ) => {
+const register = async ( req = request, res = response ) => {
 
-	res.json( {
-		ok: true,
-		user: req.body,
-	} );
+	try {
+		const { email } = req.body;
+		let user = await User.findOne( { email } );
+
+		if ( user ) {
+			return res.status( 400 ).json( {
+				ok: false,
+				message: 'El correo ya está registrado'
+			} );
+		}
+
+		user = new User( req.body );
+
+		await user.save();
+
+		res.status( 201 ).json( {
+			ok: true,
+			user: req.body,
+		} );
+
+	} catch ( e ) {
+		console.log( e );
+		res.status( 500 ).json( {
+			ok: false,
+			message: 'Error al registrar el usuario'
+		} );
+	}
+
+
 };
 
 const login = ( req = request, res = response ) => {
